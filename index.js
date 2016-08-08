056cecc13264a458d66e170ff19d8b3fed2e81fd
Lenny Castaneda <lenny.castaneda@gmail.com>
2016-08-08 10:49:45 -0700

    added a few css, classa nd id changes

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 $(document).ready(function() {
-	"use strict";  //Allows error feedback from any syntax error in my code so I can catch those error...Strict mode declared at beginning of script = global scope (all code in the script will execute in strict mode)...With strict mode, you can not use undeclared variables. If I didn't have this I wouldn't know if there was any misspelled variables that could linger in the code wihtout my awareness.
-
+	//Allows error feedback from any syntax error in my code so I can catch those error...Strict mode declared at beginning of script = global scope (all code in the script will execute in strict mode)...With strict mode, you can not use undeclared variables. If I didn't have this I wouldn't know if there was any misspelled variables that could linger in the code wihtout my awareness.
+	"use strict";  
 	$.getJSON("topspots.json",function(tableList) {
 		//Get JSON array list
-		var objectList = tableList; // assign tableList to objectList
-		var tableBody = $("<tbody><tbody>");   //assign jQuery call <tbody> tags to  tableBody variable.
+		// assign tableList to objectList
+		var objectList = tableList; 
+		//assign jQuery call <tbody> tags to  tableBody variable.
+		var tableBody = $("<tbody><tbody>");
 
 		//Iterate across each JSON oject
 		$.each(objectList, function(i){
@@ -17,9 +19,8 @@ $(document).ready(function() {
 			protoObject.name = object.name;
 			protoObject.description = object.description;
 	
-
 			//Connect Latitude and Longitude to the clickable Google maps link
-			var link = $('<a class="center-block button btn btn-success" target="blank">Open in Google Maps</a>');
+			var link = $('<a class="center-block button btn btn-success .col-xs-12 .col-md-8" target="blank">Open in Google Maps</a>');
 
 			var googleMaps = "https://www.google.com/maps?q=";
 			//Taking the JSON object list table and looking inside the "location" property grabbing the first index containing the latitude value then going in index 2 of that location array and grabbing that longtitude value, contcatinating and adding those value pairs to googleMaps vairable.
@@ -38,10 +39,8 @@ $(document).ready(function() {
 			});
 			//Append entire itteration into table rows
 			tableBody.append(row);
-		
 		});
 		//Create the table body with generated content.
 		$("thead").after(tableBody);
-
 	});
 });
